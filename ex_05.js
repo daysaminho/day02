ba68aee2ab686738c4793cb62f0a3699c36a3fba
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-01 14:24:20 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,14 @@
+let straws = 3
+let apples = 2
+let limes = 2
+let peach = 1
+let pears = 2
+
+let array = ["straws", "straws", "straws", "apples", "apples", "limes", "limes", "peach", "pears", "pears"]
+
+function getBasketContent() { 
+return array
+
+}
+
+console.log(array.length)
