6c02c1dde7b470c12ef36b249867effbc2276074
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-01 15:43:41 +0200

    Create ex_05bis.js

---
+++ ex_05bis.js
@@ -0,0 +1,28 @@
+let array = ["straws", "straws", "straws", "apples", "apples", "limes", "limes", "peach", "pears", "pears"]
+
+function getBasketContent() { 
+return array
+
+}
+
+
+let number = 13
+
+console.log(number-array.length)
+
+if (number > array.length) {
+
+
+
+
+
+
+
+console.log("Too many fruit(s)selected")
+
+}
+
+array.splice(0, number)
+
+
+getBasketContent();
