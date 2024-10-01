2cc0b0f5c12989b0dbb1d759eb1b69cb920babb4
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-01 11:03:28 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,15 @@
+function changebackground() {
+  
+ let  magicNumber = 1;
+  if (magicNumber === 1) {
+    paintYellowRectangle();
+      magicNumber = 2;
+  }  if (magicNumber === 2) {
+    paintGreenRectangle();
+    magicNumber = 3;
+  }  if (magicNumber === 3) {
+    paintRedRectangle();
+  }
+}
+
+changebackground();
